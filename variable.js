ab41fb3cc16a26f14676d86e78259f79fa1a8983
spiralhardik <148972419+spiralhardik@users.noreply.github.com>
2025-06-25 07:49:50 +0000

    varibale in js

--- variable.js
+++ variable.js
@@ -2,8 +2,8 @@ const name = "hardik";
 let age = 25;
 var designation = "web designer";
 city ="Surat"
-
+pincode = "362130"
 
 age = "35"
 city = "Junagadh"
-console.table([ name , age, designation, city]);
+console.table([ name , age, designation, city, pincode]);
