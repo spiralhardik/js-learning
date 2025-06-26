8282efba5fa8a4bb747f53439a0259496b64b511
spiralhardik <148972419+spiralhardik@users.noreply.github.com>
2025-06-26 04:29:13 +0000

    datatyoes

---
+++ 02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all js code new veson of JS
+
+//How many datatypes we have
+
+// sting -> ""
+// number -> 123
+// boolean -> True / False
+// bigint =
+// null -> stand alone value
+// undefined ->  unique
+
+
+//object = 
