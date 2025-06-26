7547e61418e0c7cbb778574ed4665051edd7fe82
spiralhardik <148972419+spiralhardik@users.noreply.github.com>
2025-06-26 04:53:23 +0000

    coversionOpretions

---
+++ conversionOpretion.js
@@ -0,0 +1,17 @@
+const score  = undefined;
+
+const value = Number (score)
+
+// console.log(typeof value);
+// console.log(value);
+
+let isLoginn = 33;
+
+let isNumber = String (isLoginn);
+
+console.log(isNumber);
+
+
+
+
+
